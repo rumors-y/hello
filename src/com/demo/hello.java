91296b5bf3d4c19e843e6e5d334936f41444e51a
rumors-y <yi04052917>
2020-07-08 18:48:36 +0800

    hello

---
+++ src/com/demo/hello.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+/**
+ * @program: hello
+ * @description:
+ * @author: y
+ * @create: 2020-07-08 18:44
+ **/
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
